330a5625b533ea6999fd406e9e280a799a7917df
Geldux <harrybrooklyn1994@gmail.com>
2026-05-29 01:43:16 +0530

    Fix mobile Expo SDK 51 dependency alignment

---
+++ movenrun/mobile/metro.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config");
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "..");
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+
+config.resolver.disableHierarchicalLookup = false;
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
